191fd993532acd16238fa6fdf0c4161d99253b3f
RuralPups <xulincq163@163.com>
2015-01-25 00:02:27 +0800

    新增登录页面

---
+++ src/com/xulin/app/activity/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.xulin.app.activity;
+
+import com.xulin.app.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+	}
+}
